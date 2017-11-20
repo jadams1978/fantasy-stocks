3ad14643e6e13cd9351bd108b821a63412f5ec98
Tzikas <niko.tzikas@gmail.com>
2017-11-20 13:48:28 -0500

    fixed promises and api speed limit

--- update-team-score.js
+++ update-team-score.js
@@ -12,52 +12,12 @@ const mongoose = require('mongoose');
 const PromiseThrottle = require('promise-throttle');
 
 var promiseThrottle = new PromiseThrottle({
-    requestsPerSecond: .5,           // up to 1 request per second 
+    requestsPerSecond: .777,           // up to 1 request per second 
 });
 
 
-mongoose.Promise = bluebird;
-mongoose.connect('mongodb://localhost/stocks');
+//mongoose.Promise = bluebird;
 //mongoose.connect('mongodb://john:fantasystocks@ds155695.mlab.com:55695/fantasystocks');
-console.log('update');
-
-
-function fetchData(stockname) {
-    console.log('fetching data', stockname);
-    return fetch(`https://www.quandl.com/api/v3/datasets/${stockname}/data.json?api_key=RHAbp4b2msadmufSJuzn`)
-        .then(function(res) {
-            return res.json();
-	}).then(function(data) {
-	    console.log('data',data.quandl_error)
-
-	    if(!data.dataset_data){ return null}
-	    //console.log('data',data)
-            let open = data.dataset_data.data[0][1];
-            let close = data.dataset_data.data[0][4];
-            let profit = close - open;
-            console.log('the profit for ' + stockname + "  is " + profit);
-            
-            return profit;
-	}).catch(function(err){
-   	        console.log('err', err)
-		throw err; 
-	});
-}
-
-
-
- var myFunction = function(i) {
-    return new Promise(function(resolve, reject) {
-      // here we simulate that the promise runs some code 
-      // asynchronously 
-      setTimeout(function() {
-        console.log(i + ": " + Math.random());
-        resolve(i);
-      }, 10);
-    });
-  };
-
-
 
 
  var fetchFunction = function(stockname) {
@@ -71,25 +31,18 @@ function fetchData(stockname) {
             return res.json();
 	}).then(function(data) {
 	    console.log('data',data.quandl_error)
-
 	    if(!data.dataset_data){ return null}
 	    //console.log('data',data)
             let open = data.dataset_data.data[0][1];
             let close = data.dataset_data.data[0][4];
             let profit = close - open;
-            console.log('the profit for ' + stockname + "  is " + profit);
-            
+            console.log('the profit for ' + stockname + "  is " + profit);            
 	    return profit;
-	    //resolve(stockname);
 
 	}).catch(function(err){
    	        console.log('err', err)
 		throw err; 
 	});
-
-
-
-   // });
   };
 
 
@@ -98,10 +51,6 @@ function updateTeams(team) {
 
     let totals = [];
     team.stocks.forEach(function(stock, i) {
-       // totals.push(fetchData(stock.name));
-	//promiseThrottle.add(myFunction.bind(this, 1)
-	//totals.push(promiseThrottle.add(fetchData(stock.name).bind(this, i)));
-	//totals.push(promiseThrottle.add(fetchFunction.bind(this, stock.name)))
 	totals.push(promiseThrottle.add(fetchFunction.bind(this, stock.name)))
 	
     })
@@ -140,24 +89,11 @@ function updateTeams(team) {
 
 
 
-function updateLeague(league_id){
+function updateLeague(league){
 
+    return new Promise(function(resolve, reject) {
 
-
-}
-
-function calculateScores() {
-    console.log('hi omething');
-    let league_id = "";
-    League
-        .find()
-        .exec()
-        .then(leagues => {
-            console.log(leagues);
-
-            leagues.forEach(function(league, i) {
-            league_id = league._id;
-
+         	let league_id = league._id;
 
                 Team
                     .find({
@@ -174,51 +110,61 @@ function calculateScores() {
 
                             let now = new Date();
                             league.schedule.forEach(function(week) {
-
-
                                 if (now >= week.date && now < now.addDays(1)) {
-
                                     week.games.forEach(function(game) {
                                         game.matchups.forEach(function(match) {
-
-
                                             var result = l.filter(function(obj) {
-                                                //console.log(obj)
                                                 return obj.teamname == match.team.teamname;
                                             });
-                                            //console.log(result,'result')
                                             match.team.score = result[0].score;
                                         })
                                     })
                                 }
                             })
-
                             league.markModified('schedule');
-
                             league.save();
-
-
                             console.log('calcfunction', new Date());
                             console.log(' ');
                             console.log(' ');
                             console.log(' ');
                             console.log(' ');
-                            console.log(' ');
-                            setTimeout(function() {
+			    console.log(' ');
+			    resolve()
+			    //setTimeout(function() {
 				//mongoose.connection.close();
-                                console.log("end bucs");
+				//console.log("end bucs");
 
-                            },2000)
+			    //},2000)
                             
                         })
-                        console.log('end bengals');
+		    })
+    })
+}
+
+function calculateScores() {
+    mongoose.connect('mongodb://localhost/stocks');
+    let everything = []; 
+    League
+        .find()
+        .exec()
+        .then(leagues => {
+            console.log(leagues);
+
+            leagues.forEach(function(league, i) {
+	 	   everything.push(updateLeague(league))
+
+	    })
+            Promise.all(everything).then(l => {
+		console.log('all done', l)
+		setTimeout(function() {
+			console.log('close mongo') 
+			mongoose.connection.close();
+
+		},5000)
 
 
-                    })
-                    console.log('end seahawks');
+	    })
 
-            })
-            console.log('end pats');
         })
 }
 
